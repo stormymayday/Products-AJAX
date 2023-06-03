e5c0e3559bd6f8d5af03896dbf76b7d39e4a65d7
stormymayday <stormymayday7@gmail.com>
2023-06-03 12:16:24 -0700

    Selecting '.products-center' div. Loading, display, and error will be toggled here

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 import getElement from "./utils/getElement.js";
+
+// Selections
+// Element where the states (loading, display, error) are toggled:
+const productsCenter = getElement('.products-center');
+console.log(productsCenter);
