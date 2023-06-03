2449f16139901cace32bd782932b95024a1bc2dd
stormymayday <stormymayday7@gmail.com>
2023-06-03 12:25:32 -0700

    Imorting and invoking fetchProducts in app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import getElement from "./utils/getElement.js";
+import fetchProducts from "./utils/fetchProducts.js";
 
 // Selections
 // Element where the states (loading, display, error) are toggled:
 const productsCenter = getElement('.products-center');
-console.log(productsCenter);
+
+fetchProducts();
