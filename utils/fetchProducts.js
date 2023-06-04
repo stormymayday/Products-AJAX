539b6944986127fd51979c69adf3fd3c72d90fef
stormymayday <stormymayday7@gmail.com>
2023-06-04 11:28:45 -0700

    Selecting '.products-center' div from fetchProducts.js

--- utils/fetchProducts.js
+++ utils/fetchProducts.js
@@ -2,6 +2,10 @@ import getElement from "./getElement.js";
 
 const url = 'https://course-api.com/javascript-store-products';
 
+// Element selections
+const productsCenter = getElement('.products-center');
+console.log(productsCenter);
+
 const fetchProducts = async () => {
 
     try {
