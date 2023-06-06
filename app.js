762e04d896e912980ad9f090cbc1753e9b16a8ee
stormymayday <stormymayday7@gmail.com>
2023-06-05 21:39:44 -0700

    Removed the fetchProducts import

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 import getElement from "./utils/getElement.js";
-import fetchProducts from "./utils/fetchProducts.js";
 import start from "./utils/start.js";
 
 // Selections
 // Element where the states (loading, display, error) are toggled:
 const productsCenter = getElement('.products-center');
 
-// fetchProducts();
 start();
