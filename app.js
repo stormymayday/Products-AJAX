9f525203cde6e2932542260124ec3def5b5a4086
stormymayday <stormymayday7@gmail.com>
2023-06-05 21:34:50 -0700

    Importing start into app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import getElement from "./utils/getElement.js";
 import fetchProducts from "./utils/fetchProducts.js";
+import start from "./utils/start.js";
 
 // Selections
 // Element where the states (loading, display, error) are toggled:
