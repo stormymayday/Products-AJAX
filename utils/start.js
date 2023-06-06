39a5a47a51cb6d8b45d02231d2e3de372c52eb5e
stormymayday <stormymayday7@gmail.com>
2023-06-05 21:38:01 -0700

    Invoking displayProducts passing in the 'data'

--- utils/start.js
+++ utils/start.js
@@ -1,8 +1,11 @@
 import fetchProducts from "./fetchProducts.js";
+import displayProducts from "./displayProducts.js";
 
 const start = async () => {
 
     const data = await fetchProducts();
+
+    displayProducts(data);
 
 };
 
