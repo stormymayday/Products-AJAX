cd115564fbf4318d8d7c2dfb437988f69cd182de
stormymayday <stormymayday7@gmail.com>
2023-06-10 08:11:23 -0700

    Defined start.js for single product

--- singleProduct/start.js
+++ singleProduct/start.js
@@ -1,9 +1,14 @@
-import getElement from "../utils/getElement.js";
+import getElement from "../utils/getElement";
 import fetchProduct from "./fetchProduct.js";
 import displayProduct from "./displayProduct.js";
-
 
 const productDOM = getElement('.product');
 const url = 'https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog';
 
-console.log(displayProduct(productDOM));
+const start = async () => {
+
+    displayProduct(productDOM);
+
+};
+
+export default start;
