d7c5f4933c9d833e5ed2fd846dcece173c0b5af4
stormymayday <stormymayday7@gmail.com>
2023-06-10 07:55:29 -0700

    Importing fetchProduct.js into product.js

--- utils/product.js
+++ utils/product.js
@@ -1,4 +1,5 @@
 import getElement from "./getElement.js";
+import fetchProduct from "./fetchProduct.js";
 
 const productDOM = getElement('.product');
 const url = 'https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog';
