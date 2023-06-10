5a798153e61f67abe3bd27a602c011997813c67b
stormymayday <stormymayday7@gmail.com>
2023-06-10 08:04:22 -0700

    Fixed product.js link

--- singleProduct/product.js
+++ singleProduct/product.js
@@ -1,5 +1,6 @@
 import getElement from "../utils/getElement.js";
 import fetchProduct from "./fetchProduct.js";
 
+
 const productDOM = getElement('.product');
 const url = 'https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog';
