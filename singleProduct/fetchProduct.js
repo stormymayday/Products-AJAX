94b757ebaa6fd4eed03492cceba4f705515534ae
stormymayday <stormymayday7@gmail.com>
2023-06-11 21:04:55 -0700

    Improting getElement and selecting '.product' section

--- singleProduct/fetchProduct.js
+++ singleProduct/fetchProduct.js
@@ -1,6 +1,11 @@
+import getElement from "../utils/getElement.js";
+
+const productDOM = getElement('.product');
+const url = 'https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog';
+
 const fetchProduct = async () => {
 
-    return `product`;
+    return productDOM;
 
 };
 
